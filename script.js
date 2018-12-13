1b355f2fe241a7f45d2a0703e7bf3ac1f60c088f
YuraSadilenko <yura.sadilenko.it@gmail.com>
2018-12-13 22:42:50 +0200

    fixes

--- script.js
+++ script.js
@@ -3,59 +3,67 @@
 //  myGroup = 'fe-oct18',
 //  myComrads = ['Alina','Yura', 'Vadim', 'Dima', 'Serega'];
 
-var discount = function hour(question){
-  var val = +prompt(question, '');
-  if (isNaN(val) || val < 0 || val > 23) {
+var discount = function hour(question) {
+  var value = +prompt(question, '');
+  if (isNaN(value) || value < 0 || value > 23) {
     alert('Incorrect value');
-    hour(question, '');
+    value = hour(question, '');
   }
-  return val;
+  return value;
 };
 
 var saleStart = discount('dicount start?');
 var saleEnd = discount('discount end?');
 var time = discount('current time?');
 
-if ( saleStart < saleEnd && time >= saleStart && time <= saleEnd) {
-  alert('this is morning dicount time');
-} else if ( saleStart > saleEnd && ( time >= saleStart || time <= saleEnd ) ) {
-  alert('this is evening dicount time');
+console.log(saleStart);
+console.log(saleEnd);
+console.log(time);
+
+
+if (saleStart < saleEnd && time >= saleStart && time <= saleEnd) {
+  console.log('this is morning dicount time');
+} else if (saleStart > saleEnd && ( time >= saleStart || time <= saleEnd )) {
+  console.log('this is evening dicount time');
 } else {
-  alert('no discount');
+  console.log('no discount');
 }
 
-//  var money = prompt('How much money do you have?');
-//  var question;
+// var money = prompt('How much money do you have?');
+// var question;
 
-// if(money >= 100) {
+// if (money) {
+//   console.log('You dont buy anything.');
+// }
+
+// if (money >= 100) {
 //   question = confirm('You can buy BigMac Menu');
-//   if( question ) {
+//   if (question) {
 //     alert('here is your Big Mac');
 //   } else {
 //     alert('you dont want to buy');
 //   }
 
-// }else if( money >= 50 && money < 100 ) {
+// } else if (money >= 50) {
 //   question = confirm('You can buy fry potato. and cheeseburger. Would you like to buy it?');
-//   if( question ) {
+//   if (question) {
 //     alert('here is you potato with cheeseburger');
 //   } else {
 //     alert('you dont want to buy');
 //   }
 
-// }else if( money >= 20 && money <= 50 ) {
+// } else if (money >= 20) {
 //   question = confirm('You can buy cola. Would you like to buy it?');
-//   if( question ) {
+//   if (question) {
 //     alert('here is your cola');
 //   } else {
 //     alert('you dont want to buy');
 //   }
-
-// }else if (money === null) {
-//   console.log('You dont buy anything.');
-// }else if ( money < 20 ) {
+// } else {
 //   console.log('sorry! We dont have any food this price');
 // }
+
+
 
  
 
